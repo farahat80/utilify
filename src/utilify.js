10479f665b18f48edd51cf31f2a33885af30e1e4
Ahmed Farahat <farahat80@gmail.com>
2017-03-28 17:01:51 +0200

    add extend function

--- src/utilify.js
+++ src/utilify.js
@@ -21,8 +21,24 @@ export function inArray(array, value) {
     }
   }
   catch (err) {
-    
     console.log(err.name, err.message);
   }
 }
 
+export function extend(obj1, obj2) {
+  Object.keys(obj2).forEach(function (key) { obj1[key] = obj2[key]; });
+  return obj1;
+}
+
+/**
+ * Get the value of a query string from a URL
+ * @param  {String} field The field to get the value of
+ * @param  {String} url   The URL to get the value from [optional]
+ * @return {String}       The value
+ */
+export function getQueryString(field, url) {
+  var href = url ? url : window.location.href;
+  var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+  var string = reg.exec(href);
+  return string ? string[1] : null;
+};
