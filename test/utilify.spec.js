7a4412178c9d13a866db1ef84d4aa1787a08b35c
Ahmed Farahat <farahat80@gmail.com>
2017-03-29 14:16:58 +0200

    test getParam

--- test/utilify.spec.js
+++ test/utilify.spec.js
@@ -2,6 +2,7 @@
 
 import { expect, assert } from 'chai';
 import should from 'should';
+import sinon from 'sinon';
 import utilify from '../src/utilify';
 
 describe('#trim', function () {
@@ -29,12 +30,24 @@ describe('#inArray', function () {
 });
 
 describe('#extend', function () {
-  var obj1 = { name: 'Ahmed' };
-  var obj2 = { age: 36 };
+  var obj1 = { foo: 'foo1' };
+  var obj2 = { bar: 'bar1' };
   it('should return one object of the combined objects properties', function () {
-    expect(utilify.extend(obj1, obj2)).to.deep.equal({name:'Ahmed', age:36});
+    expect(utilify.extend(obj1, obj2)).to.deep.equal({foo:'foo1', bar: 'bar1'});
   });
   it('should throw an error if the array is undefined', function () {
     expect(() => { utilify.extend(null,null) }).to.throw(Error, "input objects can't be undefined");
   });
 });
+
+describe('#getParam', function () {
+  it('should return the parameter value from a provided url string', function () {
+    expect(utilify.getParam("param1", "http://www.foo.com/?param1=hello")).to.equal("hello");
+  });
+  it('should return the parameter value from a browser url', function () {
+    var stub = sinon.stub(utilify, 'getHref').returns('http://www.foo.com?param1=hello');
+    expect(utilify.getParam("param1")).to.equal("hello");
+  });
+});
+
+
