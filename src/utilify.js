3c4a379c4c4cb3caf3618067fa8dc9e84ee194ff
Ahmed Farahat <farahat80@gmail.com>
2017-03-29 13:32:25 +0200

    refactor

--- src/utilify.js
+++ src/utilify.js
@@ -13,25 +13,25 @@ var Utilify = (function () {
   }
 
   Utilify.prototype.inArray = function (array, value) {
-    try {
-      if (array) {
-        var length = array.length;
-        for (var i = 0; i < length; i++) {
-          if (array[i] == value) return true;
-        }
-        return false;
-      } else {
-        throw new Error("Array input is undefined");
+    if (array) {
+      var length = array.length;
+      for (var i = 0; i < length; i++) {
+        if (array[i] == value) return true;
       }
-    }
-    catch (err) {
-      console.log(err.name, err.message);
+      return false;
+    } else {
+      throw new Error("Array input is undefined");
     }
   }
 
   Utilify.prototype.extend = function extend(obj1, obj2) {
-    Object.keys(obj2).forEach(function (key) { obj1[key] = obj2[key]; });
-    return obj1;
+    if (obj1 && obj2) {
+      Object.keys(obj2).forEach(function (key) { obj1[key] = obj2[key]; });
+      return obj1;
+    }
+    else {
+      throw new Error("input objects can't be undefined");
+    }
   }
 
   return Utilify;
