3148c87099834b9e86bd407319414c5b1671644c
Ahmed Farahat <farahat80@gmail.com>
2017-03-29 12:51:09 +0200

    change to module pattern

--- src/utilify.js
+++ src/utilify.js
@@ -1,44 +1,40 @@
 'use strict';
 
-export function trim(str) {
-  if (str) {
-    return str.replace(/^\s+|\s+$/gm,'');
-  } else {
-    return '';
-  }
-}
 
-export function inArray(array, value) {
-  try {
-    if (array) {
-      var length = array.length;
-      for (var i = 0; i < length; i++) {
-        if (array[i] == value) return true;
-      }
-      return false;
+var Utilify = (function () {
+  function Utilify() { }
+
+  Utilify.prototype.trim = function (str) {
+    if (str) {
+      return str.replace(/^\s+|\s+$/gm, '');
     } else {
-      throw new Error("Array input is undefined");
+      return '';
     }
   }
-  catch (err) {
-    console.log(err.name, err.message);
+
+  Utilify.prototype.inArray = function (array, value) {
+    try {
+      if (array) {
+        var length = array.length;
+        for (var i = 0; i < length; i++) {
+          if (array[i] == value) return true;
+        }
+        return false;
+      } else {
+        throw new Error("Array input is undefined");
+      }
+    }
+    catch (err) {
+      console.log(err.name, err.message);
+    }
   }
-}
 
-export function extend(obj1, obj2) {
-  Object.keys(obj2).forEach(function (key) { obj1[key] = obj2[key]; });
-  return obj1;
-}
+  Utilify.prototype.extend = function extend(obj1, obj2) {
+    Object.keys(obj2).forEach(function (key) { obj1[key] = obj2[key]; });
+    return obj1;
+  }
 
-/**
- * Get the value of a query string from a URL
- * @param  {String} field The field to get the value of
- * @param  {String} url   The URL to get the value from [optional]
- * @return {String}       The value
- */
-export function getQueryString(field, url) {
-  var href = url ? url : window.location.href;
-  var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
-  var string = reg.exec(href);
-  return string ? string[1] : null;
-};
+  return Utilify;
+})();
+
+module.exports = new Utilify();
