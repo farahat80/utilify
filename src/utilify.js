f18b4002b483203dd7de6609114e3b55c18ff379
Ahmed Farahat <farahat80@gmail.com>
2017-06-22 10:35:39 +0200

    adding the numbers class

--- src/utilify.js
+++ src/utilify.js
@@ -2,13 +2,14 @@ import Strings from './utilify.strings';
 import Arrays from './utilify.arrays';
 import Objects from './utilify.objects';
 import Url from './utilify.url';
-
+import Numbers from './utilify.numbers';
 
 var utilify = {
   strings: new Strings(),
   arrays: new Arrays(),
   objects: new Objects(),
   url: new Url(),
+  numbers: new Numbers(),
 };
 
 module.exports = utilify;
