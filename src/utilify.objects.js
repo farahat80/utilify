747980c83c0b0ac9a81ed697aa0338015fbdc5e8
Ahmed Farahat <farahat80@gmail.com>
2017-06-22 10:35:02 +0200

    adding find,search,isEqual functions

--- src/utilify.objects.js
+++ src/utilify.objects.js
@@ -14,6 +14,45 @@ var Objects = (function () {
       throw new Error('input objects can\'t be undefined');
     }
   };
+  Objects.prototype.find = function (array, key, value) {
+    var result = null;
+    array.forEach(function (item) {
+      if (item.hasOwnProperty(key)) {
+        if (item[key] === value) {
+          result = item;
+        }
+      }
+    });
+    return result;
+  };
+  Objects.prototype.search = function (array, key, value) {
+    var results = [];
+    array.forEach(function (item) {
+      if (item.hasOwnProperty(key)) {
+        if (item[key] === value) {
+          results.push(item);
+        }
+      }
+    });
+    if (results.length == 0) {
+      return null;
+    }
+    return results;
+  };
+  Objects.prototype.isEqual = function (a, b) {
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+    if (aProps.length != bProps.length) {
+      return false;
+    }
+    for (var i = 0; i < aProps.length; i++) {
+      var propName = aProps[i];
+      if (a[propName] !== b[propName]) {
+        return false;
+      }
+    }
+    return true;
+  };
 
   return Objects;
 })();
